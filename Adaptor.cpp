9c053c49c5a0f6a3c48be9174c5b31f3723323cc
Anirudh Agrawal <annumarch@gmail.com>
2020-04-24 20:30:46 +0530

    Adaptor Pattern

---
+++ Adaptor.cpp
@@ -0,0 +1,92 @@
+/*
+ * This plans to add a wrapper of sort of an existing implementaiton
+ * to provide some additional features and clearner api
+ *
+ * https://sourcemaking.com/design_patterns/adapter
+ */
+
+#include <iostream>
+#include <array>
+#include <vector>
+
+/*
+ * This class a mixture of array and vector
+ * to gain the advantage of stack for small sizes
+ */
+template <typename T>
+class VariableArray
+{
+    static const constexpr size_t MAX_ARRAY_LENGTH = 10;
+
+    std::array<T, MAX_ARRAY_LENGTH> m_array;
+    std::vector<T> m_vector;
+    size_t m_size{0};
+
+    public:
+    VariableArray() = default;
+
+    VariableArray(size_t size)
+    {
+        if (size > MAX_ARRAY_LENGTH)
+        {
+            m_vector.reserve(size - MAX_ARRAY_LENGTH);
+        }
+    }
+
+    void push_back(T const& data)
+    {
+        if (m_size < MAX_ARRAY_LENGTH)
+        {
+            m_array[m_size] = data;
+        }
+        else
+        {
+            m_vector.push_back(data);
+        }
+        m_size++;
+    }
+
+    T& back()
+    {
+        if (m_size <= MAX_ARRAY_LENGTH)
+        {
+            return m_array[m_size-1];
+        }
+        else
+        {
+            m_vector.back();
+        }
+    }
+
+    void pop_back()
+    {
+        if (m_size == 0)
+            return ;
+
+        if (m_size > MAX_ARRAY_LENGTH)
+        {
+            m_vector.pop_back();
+        }
+        // TODO: In the array case should we call the destuctor
+        m_size--;
+    }
+
+};
+
+int main()
+{
+    VariableArray<int> va;
+
+    for (int i = 0;i < 25;i++)
+    {
+        va.push_back(i);
+    }
+
+    for (int i =0;i < 25;i++)
+    {
+        std::cout << va.back() << std::endl;
+        va.pop_back();
+    }
+    return 0;
+}
+
