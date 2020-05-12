50ee1d5c3fb67970322e9801cedeae7a1a68a0ad
Anirudh Agrawal <annumarch@gmail.com>
2020-05-12 18:25:53 +0530

    Fluent Interface

---
+++ Fluent.cpp
@@ -0,0 +1,45 @@
+/*
+ * Fluent Interface Idiom
+ * This is more of an interface rather than a design pattern. The main idea is to
+ * achieve method chaining
+ */
+
+#include <iostream>
+
+class Boat
+{
+    public:
+        Boat& setFuel(int const f)
+        {
+            fuel = f;
+            return *this;
+        }
+
+        Boat& setName(std::string const& name)
+        {
+            this->name = name;
+            return *this;
+        }
+
+        void display() const
+        {
+            std::cout << this->name << " has " << this->fuel << " litres of fuel\n";
+        }
+
+
+    private:
+        int fuel;
+        std::string name;
+};
+
+int main()
+{
+    Boat b;
+    b.setFuel(10)
+     .setName("DarkMoon")
+     .display();
+
+    return 0;
+}
+
+
