de2d7f5bc425d35102468332bf82e97eda3ba023
Anirudh Agrawal <annumarch@gmail.com>
2020-06-21 15:05:06 +0530

    Added the CRTP technique

---
+++ CRTP.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <typeinfo>
+
+template <typename Derived>
+class Base
+{
+public:
+    void Name()
+    {
+        std::cout << __PRETTY_FUNCTION__ << " calling the base function" << std::endl;
+        impl().NameImpl();
+    }
+
+private:
+    Derived& impl()
+    {
+        return static_cast<Derived&>(*this);
+    }
+    friend Derived;
+};
+
+class Derived : public Base<Derived>
+{
+    using Parent = Base<Derived>;
+
+private:
+    void NameImpl()
+    {
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+    }
+    friend Parent;
+};
+
+int main()
+{
+    Derived x;
+    x.Name();
+    return 0;
+}
