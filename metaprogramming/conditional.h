8fa20171a0e972cf4573e5b5871f1053a47a0167
Anirudh Agrawal <agrwlanirudh@gmail.com>
2024-12-20 11:54:11 +0000

    Adding a constraint to conditional

--- metaprogramming/conditional.h
+++ metaprogramming/conditional.h
@@ -1,31 +1,29 @@
-#include <utility>
 #include <cassert>
+#include <utility>
 
 /*
-* In the test example one can see benefit of this approach in template metaprogramming where the vvariable can be abstracted away
-*/
+ * In the test example one can see benefit of this approach in template
+ * metaprogramming where the vvariable can be abstracted away
+ */
 template <typename T, bool Enabled>
 struct Conditional {
-	T value_;
+  T value_;
 
-	// If the underlying type supports the following operations then we do as well.
-	Conditional() = default;
-	Conditional(Conditional const& other) = default;
-	Conditional(Conditional && other) = default;
+  // If the underlying type supports the following operations then we do as
+  // well.
+  Conditional() = default;
+  Conditional(Conditional const& other) = default;
+  Conditional(Conditional&& other) = default;
 
-	Conditional(T&& _value) : value_(std::forward<T>(_value)) {}
+  Conditional(T&& _value) : value_(std::forward<T>(_value)) {}
 
-	// TODO(anirudh): Can this interface be improved.
-	T& value() { return value_; }
+  T& value() { return value_; }
 };
 
 template <typename T>
 struct __attribute__((packed)) Conditional<T, false> {
-	Conditional() = default;
-	Conditional([[maybe_unused]] T&& _value) {}
-
-	// TODO(anirudh): Can this interface be improved.
-	T& value() { 
-		assert(false);
-	}
+  Conditional() = default;
+  Conditional([[maybe_unused]] T&& _value) {}
+  Conditional(Conditional const& other) = default;
+  Conditional(Conditional&& other) = default;
 };
--- metaprogramming/main.cc
+++ metaprogramming/main.cc
@@ -1,31 +1,38 @@
 // TODO(anirudh): This should be moved to a testing framework. Setup GoogleTest.
+#include <array>
+#include <iostream>
+
 #include "conditional.h"
 
-#include <iostream>
-#include <array>
-
 struct Stats {
-	int total = 0;
-	int ongoing = 0;
-	int waiting = 0;
-	std::array<int, 10> timestamps;
+  int total = 0;
+  int ongoing = 0;
+  int waiting = 0;
+  std::array<int, 10> timestamps;
 };
 
 template <bool EnableStats>
-struct  Executor {
-	void Process() {
-		stats_.value().ongoing++;
-	}
-	int a;
-	// NOTE: We are disabling empty base class optimization with no_unique_address.
-	[[no_unique_address]] Conditional<Stats, EnableStats> stats_;
+struct Executor {
+  void Process()
+    requires EnableStats
+  {
+    stats_.value().ongoing++;
+  }
+  int a;
+  // NOTE: We are disabling empty base class optimization with
+  // no_unique_address.
+  [[no_unique_address]] Conditional<Stats, EnableStats> stats_;
 };
 static_assert(sizeof(Executor<true>) == 56);
 static_assert(sizeof(Executor<false>) == 4);
 
 void ConditionalTests() {
-	Executor<true> executor_with_stats;
-	std::cout << executor_with_stats.stats_.value().total << std::endl;
+  Executor<true> executor_with_stats;
+  std::cout << executor_with_stats.stats_.value().total << std::endl;
+
+  Executor<false> executor_without_stats;
+  // NOTE: The following fails to build because of the constraints.
+  // executor_without_stats.Process();
 }
 
 int main() {
