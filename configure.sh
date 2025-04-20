ff2a4f0ba35087f82daef29f111c14540035082f
Anirudh Agrawal <agrwlanirudh@gmail.com>
2025-04-20 23:47:11 +0800

    conan configure and build is passing

--- configure.sh
+++ configure.sh
@@ -1,3 +1,3 @@
-mkdir build
+mkdir -p build
 conan install . --profile ./profile --output-folder=build --build=missing
-cmake -B build -G Ninja .
+cmake -DCMAKE_TOOLCHAIN_FILE="build/conan_toolchain.cmake" -DCMAKE_BUILD_TYPE="Release" -B build -G Ninja .
